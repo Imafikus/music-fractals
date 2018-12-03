bc1be43fc5d1653f76aaaa3734ecf271649de944
imafikus <aleksatesicteske@gmail.com>
2018-12-03 13:02:02 +0100

    Clean up some unnecessary comments and code

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ from math import sqrt
 
 from random import randint
 
+# --PARAMETERS--
+
+#? line width of a triangle, given in pixels
 LINE_WIDTH = 1
+
+#? length of the triangle side given in pixels, starting triangle is evensided
+TRIANGLE_SIDE_SIZE = 600
+
 RECURSION_DEPTH = 6
 IMG_SRC = "black.jpg"
+
 
 def calculate_starting_points(img, side_size):
     """
     Calculates first 3 points for the triangle
     based on the img width and height and side_size.
-    Size size is the length of the triangle.
+    Size size is the length of one side of the triangle.
 
-    Whole functions is the represents the 
+    Functions represents the 
     calculation of evensided triangle points
     given its orthocenter and side length
 
@@ -28,6 +36,7 @@ def calculate_starting_points(img, side_size):
     #print(height, width, channels)
 
     random_factor = randint(-100, 100)
+    #!calculate_scaling_factor()
 
     t_x, t_y = int((height+100)/2), int(width/2)
     #print(t_x, t_y)
@@ -41,7 +50,7 @@ def calculate_starting_points(img, side_size):
     b_y = a_y 
 
     c_x = int(t_x) + random_factor
-    c_y = int(t_y - side_size*sqrt(3) / 3) + random_factor
+    c_y = int(t_y - side_size*sqrt(3) / 3) + random_factor #+ 700
 
     return [ [a_x, a_y], [b_x, b_y], [c_x, c_y] ]  
 
@@ -59,24 +68,17 @@ def draw_sierpinski_layer(triangle, img, level):
     b_x, b_y = triangle[1]
     c_x, c_y = triangle[2]
 
-    # print(a_x, a_y)
-    # print(b_x, b_y)
-    # print(c_x, c_y)
-
     ab_x, ab_y = int((a_x + b_x) / 2), int((a_y + b_y) / 2) 
     ac_x, ac_y = int((a_x + c_x) / 2), int((a_y + c_y) / 2) 
     bc_x, bc_y = int((b_x + c_x) / 2), int((b_y + c_y) / 2)
-
     
     tr1 = [ [a_x, a_y], [ab_x, ab_y], [ac_x, ac_y] ]
     tr2 = [ [ab_x, ab_y], [b_x, b_y], [bc_x, bc_y] ]
     tr3 = [ [ac_x, ac_y], [bc_x, bc_y], [c_x, c_y] ]
 
-
     tr1_points = np.array(tr1, np.int32)  
     tr2_points = np.array(tr2, np.int32)  
     tr3_points = np.array(tr3, np.int32)  
-
 
     cv2.polylines(img,[tr1_points],True,(255, 0, 85), LINE_WIDTH)
     cv2.polylines(img,[tr2_points],True,(255, 0, 85), LINE_WIDTH)
@@ -92,15 +94,10 @@ def draw_sierpinski_layer(triangle, img, level):
     draw_sierpinski_layer(tr2, img, level)
     draw_sierpinski_layer(tr3, img, level)
 
-
 def main():
     img = cv2.imread(IMG_SRC)
-    
-    #line_thickness = 2
-    #cv2.line(img, (20, 20), (60, 20), (0, 255, 0), line_thickness)
-    
+        
     side_size = 600
-    #triangle_pts = calculate_starting_points(img, side_size)
 
     for i in range(0, 10):
         #print(randint(-100, 100))
@@ -110,17 +107,9 @@ def main():
         path = "pictures/img" + str(i) + ".png"
         cv2.imwrite(path, img)
     
-    #pts = np.array(triangle_pts, np.int32)
-
-    #pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
-    #pts = pts.reshape((-1,1,2))
-    #cv2.polylines(img,[pts],True,(0,255,255), 3)
-    
     #cv2.imshow("image", img)
     #cv2.waitKey()
     #cv2.destroyAllWindows()
-
-
 
 if __name__ == "__main__":
     main()
--- sound_processing.py
+++ sound_processing.py
@@ -2,36 +2,33 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile as wav
 from scipy.fftpack import fft
 import numpy as np
+np.set_printoptions(threshold=np.inf)
+
+SCALING_FACTOR = 200
 
 def main():
-    samp_freq, snd = wav.read('samples/440_sine.wav')
+    samp_freq, snd = wav.read('samples/bass_sample.wav')
     
     #?amplitude is mapped from -2^15 to 2^15 - 1, we want normalized amplitude
-    snd = snd /(2.0**15)
+    snd = snd /(2.0**15) * SCALING_FACTOR
     
-
-    for s in snd[:, 0]:
-        print(s)
-    return
     #?snd has 2 channels and some number of sampling points
-    #print(snd.shape)
-    sample_points = snd.shape[0]
+    #?print(snd.shape)
+    sample_points = snd.shape[0]# * SCALING_FACTOR
+    print("SAMPLE POINTS", sample_points)
 
     #?we can calculate the time points by knowing the number
     #?of sampling points and sample frequency, because
     #?sample_points / samp_freq will give as time in ms^(-3)
     #?we want to see nice representation of miliseconds so we
     #?multiply by 10^3
-    time_array = np.arange(0, sample_points, 10) / samp_freq * 1000
+    time_array = np.arange(0, sample_points, 1) / samp_freq * 1000
 
-    #fft_out = fft(data)
-    #%matplotlib inline
     plt.plot(time_array, snd, color='k')
     plt.xlabel("Time(ms)")
     plt.ylabel("Amplitude")
-    #plt.plot(data, fft_out)
     
-    #plt.show()
+    plt.show()
 
 if __name__ == "__main__":
     main()
