f2d39413d1f48cfdf43395ee2ca11e110b58b9ac
imafikus <aleksatesicteske@gmail.com>
2018-12-04 13:47:23 +0100

    Make test gif processing

--- gif_processing.py
+++ gif_processing.py
@@ -9,14 +9,14 @@ def generate_gif(duration):
     images = []
 
     #!!!FIXME, cannot be fixed value
-    for i in range(0, 176300):
-        if i % 100 == 0:
-            #!FIXME cannot have path like this
-            filename = "pictures/img" + str(i) + ".png"
-            images.append(imageio.imread(filename))
+    for i in range(0, 12):
+        #if i % 100 == 0:
+        #!FIXME cannot have path like this
+        filename = "test/img" + str(i) + ".png"
+        images.append(imageio.imread(filename))
     
     output_file = 'kurcina.gif'
-    imageio.mimsave(output_file, images, duration=duration)
+    imageio.mimsave(output_file, images, duration=0.5)
 
 
 if __name__ == "__main__":
